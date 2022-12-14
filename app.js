32186fd0f23e4374e9c4f5726bc7642fe541f5d3
guiszk <guiszk@protonmail.com>
2022-12-13 23:26:24 -0300

    Added files

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var dateFns = require('date-fns');
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+
+const { Client } = require('pg')
+const client = new Client()
+
+client.connect()
+
+app.get('/', (req, res) => {
+
+    // get all posts from db
+    query = 'SELECT * FROM posts;';
+    client.query(query, (err, pgres) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        sorted = pgres.rows.slice().sort(function (a, b) { return a.id - b.id }).reverse();
+        res.render('index', { posts: sorted });
+    });
+})
+
+// create new post
+app.post('/', urlencodedParser, function (req, res) {
+    now = new Date();
+    response = {
+        posttitle: req.body.title,
+        content: req.body.content,
+        date: dateFns.format(now, 'yyyy-MM-dd hh:mm:ss'),
+    };
+
+    const query = `
+        INSERT INTO posts (title, content, date)
+        VALUES ('${response['posttitle']}', '${response['content']}', '${response['date']}')
+    `;
+    client.query(query, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+    res.redirect("/");
+});
+
+// run locally
+/* app.listen(8080, () => {
+    console.log('listening on http://localhost:8080');
+}); */
+
+// run
+var server = app.listen(process.env.PORT, function () {
+    var host = '0.0.0.0';
+    var port = process.env.PORT || 8888;
+});
